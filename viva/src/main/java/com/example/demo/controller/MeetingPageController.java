ca878a478133751672bad27519c95ec3502aa8b7
whguswls58 <whguwls58@naver.com>
2025-07-08 16:35:38 +0900

    새로 테스트한다고 바꾸는중

--- viva/src/main/java/com/example/demo/controller/MeetingPageController.java
+++ viva/src/main/java/com/example/demo/controller/MeetingPageController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MeetingPageController {
 	@GetMapping("/interviewPage")  // 또는 "/meeting/main" 등
     public String interviewSPA() {
-        return "webRtc/interview-spa"; // templates/webRtc/interview-spa.html
+        return "webRtc/interviewTest"; // templates/webRtc/interview-spa.html
     }
 }
